6340815da9678d280fcf45325f714e90281b2c92
bukhuti-vardanidze <84092360+bukhuti-vardanidze@users.noreply.github.com>
2022-11-11 22:23:20 +0400

    Calculator using Switch

---
+++ switchCalculator/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Enter First number :");
+double number1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter Second number :");
+double number2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Choose One Of This : [+] [-] [*] [/] [%]");
+string Operation = Console.ReadLine();
+
+    switch(Operation)
+    {
+        case "+" :
+            Console.WriteLine($"{number1} + {number2} = {number1 + number2} ");
+            break;
+        case "-" :
+            Console.WriteLine($"{number1}  - {number2} = {number1 - number2} ");
+            break;
+        case "/" :
+            Console.WriteLine($"{number1} / {number2} = {number1 / number2} ");
+            break;
+        case "*" :
+            Console.WriteLine($"{number1} * {number2} = {number1 * number2} ");
+            break;
+        case "%" :
+            Console.WriteLine($"{number1} % {number2} = {number1 % number2} ");
+            break;
+        default:
+            Console.WriteLine("choose right operation!");
+            break;
+        
+       
+    }
+
